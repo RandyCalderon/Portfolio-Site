2b7f2438fc0f8ad1874815aabf0a609669d48d5b
Randy Calderon <randycweb@gmail.com>
2019-01-12 10:16:24 -0800

    About contact form update along with styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { css } from '@emotion/core'
-import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
+import { FaTwitter, FaLinkedin, FaGithub, FaPhone } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
 
 const color = css`
   margin-top: 100px;
@@ -17,9 +18,9 @@ const aboutWrapper = css`
 `
 
 const icons = css`
-  width: 100px;
+  width: 200px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   margin-left: 25px;
 `
 
@@ -27,12 +28,29 @@ const links = css`
   color: black;
 `
 
+const iconSize = css`
+  height: 35px;
+  width: 35px;
+`
+
+
 const text = css`
+display: flex;
+flex-direction: column;
   font-family: 'Ubuntu', sans-serif;
   padding-left: 30px;
   width: 700px;
   text-align:justify;
   text-justify:inter-word;
+  font-size: 20px;
+`
+const contactDetails = css`
+  display: flex;
+  flex-direction: column;
+  `
+
+const contactText = css`
+  padding-bottom: 25px;
 `
 
 const About = () => {
@@ -40,17 +58,23 @@ const About = () => {
     <Layout>
       <div css={color}>
         <div css={aboutWrapper}>
-          <img css={imgStyle} src='https://res.cloudinary.com/deqko80pg/image/upload/c_fit,h_150,r_0,w_150,x_0,y_0/v1547202723/Randy.jpg' />
+          <img css={imgStyle} src='https://res.cloudinary.com/deqko80pg/image/upload/c_fit,h_250,r_0,w_250,x_0,y_0/v1547202723/Randy.jpg' />
           <div css={icons}>
-            <a css={links} href='https://twitter.com/RandyCdev' target='__blank'><FaTwitter /></a>
-            <a css={links} href='https://www.linkedin.com/in/randy-calderon-237474161/' target='__blank'><FaLinkedin /></a>
-            <a css={links} href='https://github.com/RandyCalderon' target='__blank'><FaGithub /></a>
+            <a css={links} href='https://twitter.com/RandyCdev' target='__blank'><FaTwitter css={iconSize} /></a>
+            <a css={links} href='https://www.linkedin.com/in/randy-calderon-237474161/' target='__blank'><FaLinkedin css={iconSize} /></a>
+            <a css={links} href='https://github.com/RandyCalderon' target='__blank'><FaGithub css={iconSize} /></a>
           </div>
         </div>
         <div css={text}>
           <h2>About Me</h2>
           <p>Hi, my name is Randy Calderon and I'm a FrontEnd Web Developer located in Southern California with an interest in animation and solving the challenge of providing the best user experience programmatically. My dream is to one day be able to work in a setting where my work can affect the education model in a positive way and/or bring more awareness to our environmental issues.</p>
           <h3>Contact Me</h3>
+          <div css={contactDetails}>
+            <FaPhone css={iconSize} /> <h3>Phone</h3>
+            <span css={contactText}>818-284-5384</span>
+            <MdEmail css={iconSize} /> <h3>Email</h3>
+            <span>randycweb@gmail.com</span>
+          </div>
         </div>
       </div>
     </Layout>
