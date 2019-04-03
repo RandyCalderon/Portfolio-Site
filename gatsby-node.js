96aefb449525417d4293f1e71fac1aae296d7fea
Randy Calderon <randycweb@gmail.com>
2019-04-03 07:26:04 -0700

    Changes to gatsy-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,3 +22,14 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
+  actions.setBabelPreset({
+    name: `@emotion/babel-preset-css-prop`,
+    options: {
+      sourceMap: process.env.NODE_ENV !== `production`,
+      autoLabel: process.env.NODE_ENV !== `production`,
+      ...(pluginOptions ? pluginOptions : {}),
+    },
+  })
+}
